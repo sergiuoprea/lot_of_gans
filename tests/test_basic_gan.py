365ed33131135451bded1b939586828b794f5caf
sergiuoprea <soprea.dtic.ua.es>
2021-03-23 14:39:14 +0100

    Test function parametrized. Let's try now!

--- tests/test_basic_gan.py
+++ tests/test_basic_gan.py
@@ -1,8 +1,10 @@
 import torch
+import pytest
 from pytorch_lightning import seed_everything
 from project.basic_gan import generator_block
 
-def test_generator_block(in_features, out_features, num_test=1000):
+@pytest.mark.parametrize("in_features,out_features,num_test", [(15,12,1000), (15,18,1000)])
+def test_generator_block(in_features, out_features, num_test):
     seed_everything(0) # fixed value. do not change.
     
     block = generator_block(in_features, out_features)
@@ -20,9 +22,3 @@ def test_generator_block(in_features, out_features, num_test=1000):
     assert tuple(test_output.shape) == (num_test, out_features)
     assert test_output.std() > 0.55
     assert test_output.std() < 0.65
-
-
-if __name__ == "__main__":
-    test_generator_block(15, 12)
-    test_generator_block(15, 28)
-    
