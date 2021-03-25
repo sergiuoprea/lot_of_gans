1069a2ce3fdaa0768cb15c9761cad6210c5dee38
sergiuoprea <soprea.dtic.ua.es>
2021-03-25 17:07:36 +0100

    Generator implemented. Let's see if it passes testing

--- project/basic_gan.py
+++ project/basic_gan.py
@@ -1,25 +1,69 @@
 ## Imports
 from torch import nn
 
-## Discriminator
+########################################################
+#################### DISCRIMINATOR  ####################
+########################################################
 
 
-## Generator
+########################################################
+###################### GENERATOR  ######################
+########################################################
 def generator_block(input_dim, output_dim):
     """Function for returning a block of the generator's neural network,
     given ints input and output dimensions.
 
     Args:
-        input_dim ([int]): [the dimension of the input vector, a scalar]
-        output_dim ([int]): [the dimension of the output vector, a scalar]
+        input_dim (int): the dimension of the input vector, a scalar
+        output_dim (int): the dimension of the output vector, a scalar
 
     Returns:
-        [nn.Sequential]: [a layer of the GAN generator, featuring a linear transformation,
-                          followed by a batch normalization and finally a ReLu activation]
+        nn.Sequential: a layer of the GAN generator, featuring a linear transformation,
+                       followed by a batch normalization and finally a ReLu activation
     """
     return nn.Sequential(
         nn.Linear(input_dim, output_dim),
         nn.BatchNorm1d(output_dim),
         nn.ReLU(inplace=True)
     )
+    
+    
+class Generator(nn.Module):
+    """Class defining the Generator
 
+    Args:
+        z_dim (int): dimension of the noise vector (default: 10).
+        im_dim (int): dimension of the images used for training
+            (default: 784 as MNIST images are 28x28 pixels).
+        hidden_dim (int): the inner dimension (default: 128).
+    """
+    def __init__(self, z_dim=10, im_dim=784, hidden_dim=128):
+        super(Generator, self).__init__()
+        
+        self.generator = nn.Sequential(
+            generator_block(z_dim, hidden_dim),
+            generator_block(hidden_dim, hidden_dim * 2),
+            generator_block(hidden_dim * 2, hidden_dim * 4),
+            generator_block(hidden_dim * 4, hidden_dim * 8),
+            nn.Linear(hidden_dim * 8, im_dim),
+            nn.Sigmoid()
+        )
+        
+    def forward(self, noise):
+        """Forward pass of the generator given a noise tensor.
+
+        Args:
+            noise (tensor): noise tensor with dimensions (n_samples, z_dim).
+
+        Returns:
+            tensor: generated image from the input noise (batch_size, im_dim).
+        """
+        return self.gen(noise)
+    
+    def get_gen(self):
+        """Getter for the generator
+
+        Returns:
+            nn.Sequential: a sequential model of the generator.
+        """
+        return self.gen
--- tests/test_basic_gan.py
+++ tests/test_basic_gan.py
@@ -2,11 +2,12 @@
 import torch
 from torch import nn
 from pytorch_lightning import seed_everything
-from project.basic_gan import generator_block
+from project.basic_gan import generator_block, Generator
 
 # Testing libs
 import pytest
 
+# Testing the building blocks used in the Generator
 @pytest.mark.parametrize("in_features,out_features,num_test", [(15,12,1000), (15,18,1000)])
 def test_generator_block(in_features, out_features, num_test):
     seed_everything(0) # fixed value. do not change.
@@ -26,3 +27,23 @@ def test_generator_block(in_features, out_features, num_test):
     assert tuple(test_output.shape) == (num_test, out_features)
     assert test_output.std() > 0.55
     assert test_output.std() < 0.65
+
+# Testing the Generator
+@pytest.mark.parametrize("z_dim,im_dim,hidden_dim,num_test", [(5,10,20,10000),(20,8,24,10000)])
+def test_generator(z_dim, im_dim, hidden_dim, num_test):
+    gen = Generator(z_dim, im_dim, hidden_dim).get_gen()
+    
+    # Six blocks in the generator
+    assert len(gen) == 6
+    
+    test_input = torch.randn(num_test, z_dim)
+    test_output = gen(test_input)
+    
+    assert tuple(test_output.shape) == (num_test, im_dim) # Output shape is correct
+    # Make sure we are using a sigmoid as final layer
+    assert test_output.max() < 1
+    assert test_output.min() > 0
+    # No batchnorm in the output
+    assert test_output.std() > 0.05
+    assert test_output.std() < 0.15
+    
